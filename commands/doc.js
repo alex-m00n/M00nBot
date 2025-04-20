9dcd38d4096f1678ae67c475a4f9cdde13f85886
AlexM00n <140838530+alex-m00n@users.noreply.github.com>
2025-04-20 17:46:36 +0200

    Update doc.js

--- commands/doc.js
+++ commands/doc.js
@@ -2,7 +2,7 @@ import { SlashCommandBuilder } from 'discord.js';
 
 export const data = new SlashCommandBuilder()
     .setName('doc')
-    .setDescription('Envoie la documentaion du bot.');
+    .setDescription('Envoie la documentation du bot.');
     
 export async function execute(interaction) {
     await interaction.reply({ content: 'Je suis un bot créé par AlexM00n, prêt à répondre à tes commandes ! 🤖\nLien de la documentation : https://m00n-bot.vercel.app', flags : 64 });
