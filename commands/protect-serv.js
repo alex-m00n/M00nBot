9f42e0ce3daf944bd43ef8b1a4fe3b7968623ca1
AlexM00n <140838530+alex-m00n@users.noreply.github.com>
2025-04-22 12:45:55 +0200

    Rules update

--- commands/protect-serv.js
+++ commands/protect-serv.js
@@ -16,7 +16,7 @@ export async function execute(interaction) {
         .addFields(
             { name: "I. Traiter tout le monde avec respect", value: "Aucun harcèlement, chasse aux sorcières, sexisme, racisme ou discours de haine ne sera toléré." },
             { name: "II. Pas de contenu obscène ou soumis à une limite d'âge", value: "Qu'il s'agisse de texte, d'images ou de liens mettant en scène de la nudité, du sexe, de l'hyperviolence ou tout autre contenu explicite perturbant." },
-            { name: "III. Si tu remarques quelque chose de contraire aux règles ou qui te met dans un sentiment d'insécurité", value: "Informe-en les modérateurs." },
+            { name: "III. Si tu remarques quelque chose de contraire aux règles ou qui te met dans un sentiment d'insécurité", value: "Informes en les modérateurs." },
             { name: "IV. Pas de spam ni d'autopromotion", value: "(invitations de serveurs, publicités, etc.), sans l'autorisation d'un modérateur du serveur, y compris via les MP envoyés aux autres membres." },
             { name: "V. Ne répandez pas de fakenews", value: "Ou autres forme de propagande." },
             { name: "Si ces règles ne sont pas respecter vous serez sanctionné !", value: "L'équipe de modération" },
